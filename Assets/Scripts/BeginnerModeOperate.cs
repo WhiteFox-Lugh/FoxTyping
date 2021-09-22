7ce8a111688ba9676c7aa36867bc8da0966ef32b
Arthur-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2021-09-22 16:42:19 +0900

    fix: beginner mode button

--- Assets/Scripts/BeginnerModeOperate.cs
+++ Assets/Scripts/BeginnerModeOperate.cs
@@ -11,8 +11,40 @@ public class BeginnerModeOperate : MonoBehaviour {
 	private static int prevChapterNum = 1;
 	[SerializeField] private GameObject chapterSelect;
 
-	private static string[] practiceDataset = new string[2] {
-		"FoxTypingOfficial", "FoxTypingOfficialEnglish"
+	private static Dictionary<int, string> beginnerDatasetFileName = new Dictionary<int, string> {
+		{101, "keyboardMiddle"},
+		{102, "keyboardUpper"},
+		{103, "keyboardLower"},
+		{104, "chapter1All"},
+		{201, "alphabetLower"},
+		{202, "alphabetUpper"},
+		{203, "numberAndSymbol"},
+		{204, "chapter2All"},
+		{301, "hiragana_a"},
+		{302, "hiragana_k"},
+		{303, "hiragana_s"},
+		{304, "hiragana_t"},
+		{305, "hiragana_n"},
+		{306, "hiragana_h"},
+		{307, "hiragana_m"},
+		{308, "hiragana_y"},
+		{309, "hiragana_r"},
+		{310, "chapter3All"},
+		{401, "hiragana_ky"},
+		{402, "hiragana_sy"},
+		{403, "hiragana_ty"},
+		{404, "hiragana_ny"},
+		{405, "hiragana_hy"},
+		{406, "hiragana_my"},
+		{407, "hiragana_ry"},
+		{408, "hiragana_ltu"},
+		{409, "hiragana_small"},
+		{410, "chapter4All"},
+		{501, "shortWords"},
+		{502, "katakanaWords"},
+		{503, "jukugo"},
+		{504, "proverb"},
+		{505, "conversation"}
 	};
 
 	// Start is called before the first frame update
@@ -55,4 +87,11 @@ public class BeginnerModeOperate : MonoBehaviour {
 			KeyCheck(e.keyCode);
 		}
   }
+
+	public void OnClickButton(int number){
+		ConfigScript.GameMode = 0; // 短文モード固定
+		ConfigScript.DataSetName = beginnerDatasetFileName[number];
+		ConfigScript.Tasks = -1; // 無限回
+		Debug.Log(number);
+	}
 }
