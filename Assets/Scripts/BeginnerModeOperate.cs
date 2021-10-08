d153b2066ccb1e852b6c4315958c665907009e16
Arthur-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2021-10-09 08:30:58 +0900

    fix: prev settings saving (beginner)
    直前に練習したチャプターの Toggle を On にする

--- Assets/Scripts/BeginnerModeOperate.cs
+++ Assets/Scripts/BeginnerModeOperate.cs
@@ -99,6 +99,7 @@ public class BeginnerModeOperate : MonoBehaviour {
 		ConfigScript.GameMode = 0; // 短文モード固定
 		ConfigScript.DataSetName = beginnerDatasetFileName[buttonNumber];
 		ConfigScript.Tasks = -1; // 無限回練習できるようにするため、-1
+		prevChapterNum = buttonNumber / 100;
 		SceneManager.LoadScene("BeginnerTypingScene");
 	}
 }
